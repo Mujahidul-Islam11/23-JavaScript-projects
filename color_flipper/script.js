5d0013e0397228695d6c418310c7107225d50e4f
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-01-05 21:29:33 +0600

    Color changing functionality added

---
+++ color_flipper/script.js
@@ -0,0 +1,10 @@
+const colors = ["red", "green", "rgba(133, 122, 200)", "#f10525"];
+const btn = document.querySelector(".btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", ()=>{
+    const randomNumber = Math.floor(Math.random(0) *colors.length);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    color.style.color = colors[randomNumber];
+})
