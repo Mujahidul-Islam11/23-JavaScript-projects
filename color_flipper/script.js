a2c12722dcd9387355ffe95d7ab00b79b15946e8
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-01-06 20:14:33 +0600

    Updated color flipper functionality

--- color_flipper/script.js
+++ color_flipper/script.js
@@ -1,4 +1,4 @@
-const colors = ["red", "green", "rgba(133, 122, 200)", "#f10525"];
+const colors = ["red", "green", "rgba(133, 122, 200)", "#f10525", "#ff80ed"];
 const btn = document.querySelector(".btn");
 const color = document.querySelector(".color");
 
